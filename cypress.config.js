3573fe7cacbe89e1970e5d133b1a8cd2d2b2cb66
Brian Duarte <braianmucioduarte@gmail.com>
2024-10-01 10:47:49 -0300

    Correções

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require('cypress');
 // Altera a visualização da aplicação em termos de dimensões de altura e largura
 
 module.exports = defineConfig({
-  projectId: "1dj4np",
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
